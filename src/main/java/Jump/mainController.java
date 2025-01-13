b321d35975724608cf0fb900b5854a9f57cff7de
wonseokyoon <01088115030a@gmail.com>
2025-01-13 17:03:53 +0900

    2-2

---
+++ src/main/java/Jump/mainController.java
@@ -0,0 +1,15 @@
+package Jump;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class mainController {
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index(){
+        return "안녕하세요 sbb에 오신것을 환영합니다.";
+    }
+
+}
