3aebf0dddb40b1e4bb3c7172d3cc9b6feb783d21
wonseokyoon <01088115030a@gmail.com>
2025-01-13 16:46:57 +0900

    first commit

---
+++ src/main/java/Jump/JumpApplication.java
@@ -0,0 +1,13 @@
+package Jump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumpApplication.class, args);
+	}
+
+}
