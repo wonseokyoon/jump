d561c1ad48a6e8cc2e6ec749a8ee4e4048c09e5b
wonseokyoon <01088115030a@gmail.com>
2025-01-20 14:09:03 +0900

    exception sign-up

--- src/main/java/Jump/Controller/UserController.java
+++ src/main/java/Jump/Controller/UserController.java
@@ -5,6 +5,7 @@ import Jump.Entity.UserCreateForm;
 import Jump.Service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,8 +34,19 @@ public class UserController {
                     "2개의 패스워드가 일치하지 않습니다.");
             return "signup_form";
         }
-        userService.create(userCreateForm.getUsername(),
-                userCreateForm.getEmail(),userCreateForm.getPassword1());
+        try{
+            userService.create(userCreateForm.getUsername(),
+                    userCreateForm.getEmail(),userCreateForm.getPassword1());
+        }catch (DataIntegrityViolationException e){
+            e.printStackTrace();
+            bindingResult.rejectValue("signupFailed","이미 등록된 사용자입니다.");
+            return "signup_form";
+        }catch (Exception e){
+            e.printStackTrace();
+            bindingResult.rejectValue("signupFailed",e.getMessage());
+            return "signup_form";
+        }
+
         return "redirect:/";
     }
 }
