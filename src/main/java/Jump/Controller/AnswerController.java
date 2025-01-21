e3c1f064e3abc3838acd95c855645ce8280922fe
wonseokyoon <01088115030a@gmail.com>
2025-01-21 12:48:36 +0900

    anchor answer

--- src/main/java/Jump/Controller/AnswerController.java
+++ src/main/java/Jump/Controller/AnswerController.java
@@ -44,8 +44,8 @@ public class AnswerController {
             model.addAttribute("question",question);
             return "question_detail";
         }
-        answerService.create(question,answerForm.getContent(),siteUser);
-        return String.format("redirect:/question/detail/%s",id);
+        Answer answer=answerService.create(question,answerForm.getContent(),siteUser);
+        return String.format("redirect:/question/detail/%s#answer_%s",answer.getQuestion().getId(),answer.getId());
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -74,7 +74,8 @@ public class AnswerController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"수정 권한이 없습니다.");
         }
         answerService.modify(answer,answerForm.getContent());
-        return String.format("redirect:/question/detail/%s",answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s",answer.getQuestion().getId(),answer.getId());
+
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -95,7 +96,7 @@ public class AnswerController {
         SiteUser user=userService.getUser(principal.getName());
 
         answerService.vote(answer,user);
-        return String.format("redirect:/question/detail/%s",answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s",answer.getQuestion().getId(),answer.getId());
     }
 
 }
