ab4624f593ee004fa9f3352eb30fd031797e2523
wonseokyoon <01088115030a@gmail.com>
2025-01-21 11:24:28 +0900

    delete question

--- src/main/java/Jump/Controller/QuestionController.java
+++ src/main/java/Jump/Controller/QuestionController.java
@@ -129,5 +129,15 @@ public class QuestionController {
         return String.format("redirect:/question/detail/%s",id);
     }
 
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String questionDelete(Principal principal,@PathVariable("id") Integer id){
+        Question question=questionService.getQuestion(id);
+        if(!question.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"삭제 권한이 없습니다.");
+        }
+        questionService.delete(question);
+        return "redirect:/";
+    }
 
 }
--- src/main/java/Jump/Service/QuestionService.java
+++ src/main/java/Jump/Service/QuestionService.java
@@ -95,4 +95,8 @@ public class QuestionService {
         question.setModifyDate(LocalDateTime.now());
         questionRepository.save(question);
     }
+
+    public void delete(Question question){
+        questionRepository.delete(question);
+    }
 }
