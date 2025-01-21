dbe583fd03446a3ed6b63ffdcdfa93702d84e51b
wonseokyoon <01088115030a@gmail.com>
2025-01-21 12:30:39 +0900

    vote answer

--- src/main/java/Jump/Controller/AnswerController.java
+++ src/main/java/Jump/Controller/AnswerController.java
@@ -12,12 +12,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
@@ -83,6 +85,16 @@ public class AnswerController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"삭제 권한이 없습니다.");
         }
         answerService.delete(answer);
+        return String.format("redirect:/question/detail/%s",answer.getQuestion().getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/vote/{id}")
+    public String answerVote(Principal principal,@PathVariable("id") Integer id){
+        Answer answer=answerService.getAnswer(id);
+        SiteUser user=userService.getUser(principal.getName());
+
+        answerService.vote(answer,user);
         return String.format("redirect:/question/detail/%s",answer.getQuestion().getId());
     }
 
